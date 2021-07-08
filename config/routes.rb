c7f9d500dba64f2462cdc9dcc17263e109e83271
Bilal <bilalakbar458@gmail.com>
2021-07-08 19:19:20 +0500

    User authentication set up with custom attributes

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: {sign_in: "login", sign_up: "register", sign_out: "logout"}
+  get 'pages/home', to: "pages#home"
+  root to: "pages#home"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
